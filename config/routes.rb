37ebf55f2342b749ee2e6eece59f62b7ab10b1b8
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-05-05 10:28:23 -0400

    usersnewadded

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :new_recipe_ingredients, :only [:new, :create, :edit, :update]
+  resources :new_recipe_ingredients, only: [:new, :create, :edit, :update]
   resources :new_recipes 
-  resources :reactions, :only [:new, :create, :edit, :update, :destroy]
+  resources :reactions, only: [:new, :create, :edit, :update, :destroy]
   resources :posts
   # resources :recipe_ingredients
-  resources :ingredients, :only [:index, :new, :create, :show]
+  resources :ingredients, only: [:index, :new, :create, :show]
   resources :recipes, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
