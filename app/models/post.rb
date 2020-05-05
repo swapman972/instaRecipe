3e2652abe0b730a570dcf015cd49d12f96c642c9
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-05-05 08:26:00 -0400

    addedindexusers

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,30 @@ class Post < ApplicationRecord
     def post_react
         Reaction.all.select do |re| 
             re.post_id == self.id 
-        end 
+        end
+    end 
+
+    def total_likes 
+        post_react.map do |post|
+            post.like 
+        end.sum 
+    end 
+
+    def total_dislike 
+        post_react.map do |post|
+            post.dislike 
+        end.sum 
+    end 
+
+    def total_love 
+        post_react.map do |post|
+            post.love 
+        end.sum 
+    end 
+
+    def total_disgust 
+        post_react.map do |post|
+            post.disgust 
+        end.sum 
     end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :posts 
     has_many :new_recipe 
     has_many :recipes, through: :posts 
+  
+
 end
